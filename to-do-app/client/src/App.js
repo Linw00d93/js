30b12c4ee037bb94c5755b707e89ffe69a7f2b8d
Linwood <Linwood93@gmail.com>
2023-04-30 22:37:35 -0400

    tuff

--- to-do-app/client/src/App.js
+++ to-do-app/client/src/App.js
@@ -2,7 +2,7 @@ import ListHeader from "./components/ListHeader"
 
 const App = () => {
   return (
-    <div className="mainApp"> 
+    <div className="app"> 
     <ListHeader listName={'Holiday tick list'}/>
   
     </div>
